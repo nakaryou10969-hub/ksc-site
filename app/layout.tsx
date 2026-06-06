6c815bfafdf60689fca1cea00cfb3a5641609963
Nakaryou10969 <nakaryou10969@gmail.com>
2026-06-06 15:31:18 +0900

    remove redundant pt-20 from layout wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
       </head>
       <body className="min-h-full flex flex-col antialiased">
         <Header />
-        <div className="flex-1 pt-20">{children}</div>
+        <div className="flex-1">{children}</div>
         <Footer />
       </body>
     </html>
