9d5b592b34ff2b144fdd30b18dd507d93046b4e9
Nakaryou10969 <nakaryou10969@gmail.com>
2026-06-15 21:12:16 +0900

    Adjust typography weights

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;600;700;900&display=swap"
           rel="stylesheet"
         />
       </head>
